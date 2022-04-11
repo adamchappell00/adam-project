5f7bbb8c8e82f22eff83513461f1e520f6c9f7a5
adamchappell00 <adam.chappell00@gmail.com>
2022-04-11 15:54:25 -0500

    added JPA dependency

---
+++ src/main/java/com/example/adamproject/models/User.java
@@ -0,0 +1,16 @@
+package com.example.adamproject.models;
+
+
+import org.hibernate.annotations.DynamicUpdate;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@DynamicUpdate
+@Table(name="users")
+public class User {
+
+
+
+
+}
