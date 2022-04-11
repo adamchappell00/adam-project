d8e93ff4b508513ac394c88c5b502efc41b16ca9
adamchappell00 <adam.chappell00@gmail.com>
2022-04-11 15:48:22 -0500

    setup

---
+++ src/test/java/com/example/adamproject/AdamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
